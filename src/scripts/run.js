7af74c6f7fd61f7ac4f309263e5a74e8c4dca564
Phaj Nướng <baonguyenyam@gmail.com>
2018-09-06 15:00:58 +0700

    add source

---
+++ src/scripts/run.js
@@ -0,0 +1,17 @@
+// function OnRun($rootScope, AppSettings) {
+// 	'ngInject';
+
+// 	$rootScope.$on('$stateChangeSuccess', (event, toState) => {
+// 		$rootScope.pageTitle = '';
+
+// 		if (toState.title) {
+// 			$rootScope.pageTitle += toState.title;
+// 			$rootScope.pageTitle += ' \u2014 ';
+// 		}
+
+// 		$rootScope.pageTitle += AppSettings.appTitle;
+// 	});
+
+// }
+
+// export default OnRun;
