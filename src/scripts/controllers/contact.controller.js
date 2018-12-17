2117396cf5f94ae60ed4e5240e88959d62521494
Bao Nguyen <baonguyenyam@gmail.com>
2018-12-17 19:35:51 +0700

    ok

--- src/scripts/controllers/contact.controller.js
+++ src/scripts/controllers/contact.controller.js
@@ -1,5 +1,20 @@
 angular.module('contact.controller', []).controller("ContactController", _contactController)
 
-function _contactController($scope) {
-	$scope.contact = 'contactcontactcontactcontactcontactcontact'
+function _contactController($scope, $http) {
+	$scope.submit = function () {
+		let datatmp = "username=admin&password=admin"
+		$http({
+			method: 'POST',
+			url: 'http://localhost:7000/check',
+			cache: false,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			data: datatmp,
+		}).then(function (response) {
+			console.log(response)
+		}, function (error) {
+			console.log('Lỗi 000 - Main: ' + error);
+		});
+	}
 }
